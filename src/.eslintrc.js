a5f29c032881ecd2951173cbdc7021634b4a2293
Pierre Delclaux <pimise@gmail.com>
2022-08-19 11:17:10 +0200

    MEDIUM: Linters config files and related packages updates
    In order to start this cleanup, some configuration files were added to
    be able to format and lint the code with Prettier, ESLint and Vetur (for
    Vue). Some packages were updated and added to allow Babel to parse
    nullish coalescing and optional chaining operators.
    
    In order to get more modern and up to date formatting rules and best
    practices, some module updates were required. Mainly ESLint, vue-cli
    plugins and vue-cli plugins, with additionnal babel plugins in order to
    parse some operators missing from it.
    
    Fixed up JTH:
    WIP: add eslintrc in src (BDO)
    
    Fixed up JTH:
    BIG/MINOR: Fix Eslint (APO, adjust vue/max-attributes-per-line)

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -4,12 +4,37 @@ module.exports = {
         node: true,
     },
     parserOptions: {
-        parser: 'babel-eslint',
+        parser: '@babel/eslint-parser',
+        sourceType: 'module',
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:vue/essential',
-    ],
+    extends: ['eslint:recommended', 'plugin:vue/recommended', 'prettier'],
     rules: {
+        'vue/html-indent': [
+            'error',
+            4,
+            {
+                attribute: 1,
+                baseIndent: 1,
+                closeBracket: 0,
+                alignAttributesVertically: true,
+                ignores: [],
+            },
+        ],
+        'vue/max-attributes-per-line': [
+            'error',
+            {
+                singleline: {
+                    max: 8,
+                },
+                multiline: {
+                    max: 8,
+                },
+            },
+        ],
+        indent: ['error', 4, { SwitchCase: 1 }],
+        'vue/multi-word-component-names': 'off',
     },
+    plugins: [
+        'babel',
+    ],
 };
