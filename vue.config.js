5a17b3636c4c9435115b11e298097a49ec8a79aa
Pierre Delclaux <pimise@gmail.com>
2022-08-22 10:48:51 +0200

    CLEANUP: Reindent vue.config.js to 4 spaces

--- vue.config.js
+++ vue.config.js
@@ -2,18 +2,16 @@ process.env.VUE_APP_NAME = process.env.VUE_APP_NAME ?? require('./package.json')
 process.env.VUE_APP_VERSION = process.env.VUE_APP_VERSION ?? require('./package.json').version;
 
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ],
-  "publicPath": '',
-  css: {
-    extract: false,
-  },
-  chainWebpack: config => {
-      config.module
-        .rule('images')
-          .use('url-loader')
+    transpileDependencies: ['vuetify'],
+    publicPath: '',
+    css: {
+        extract: false,
+    },
+    chainWebpack: (config) => {
+        config.module
+            .rule('images')
+            .use('url-loader')
             .loader('url-loader')
-            .tap(options => Object.assign(options, { limit: 10240 }))
-  },
+            .tap((options) => Object.assign(options, { limit: 10240 }));
+    },
 }
