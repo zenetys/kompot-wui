2938e5e396ca6c0a49d03926b78bc9f840c67215
Pierre Delclaux <pimise@gmail.com>
2022-09-16 10:04:18 +0200

    MAJOR: Review webpack and Vue absolute paths aliases
    In order to use absolute paths for imports in Javascript or HTML
    templates, Webpack's config & Vue-cli's config were both updated:
    - "@": alias for "/wui/src"
    - "@wui/public": alias for "/wui/public" (mainly for config.json)

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 process.env.VUE_APP_NAME = process.env.VUE_APP_NAME ?? require('./package.json').name;
 process.env.VUE_APP_VERSION = process.env.VUE_APP_VERSION ?? require('./package.json').version;
 
@@ -13,5 +15,8 @@ module.exports = {
             .use('url-loader')
             .loader('url-loader')
             .tap((options) => Object.assign(options, { limit: 10240 }));
+        config.resolve.alias
+            .set('@', path.resolve(__dirname, './src'))
+            .set('@public', path.resolve(__dirname, './public/'));
     },
 }
