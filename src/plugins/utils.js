07168ec9a886437b80cf31d21741f0b6931f5660
Julien Thomas <jthomas@zenetys.com>
2023-06-27 19:28:06 +0200

    MINOR: utils: Remove unused or dead code

--- src/plugins/utils.js
+++ src/plugins/utils.js
@@ -13,32 +13,10 @@ export function frenchFormat(date) {
     return new Date(date).toLocaleDateString('fr-FR') + ' ~ ' + new Date(date).toLocaleTimeString('fr-FR');
 }
 
-// Format: month year (2m3y)
-export function simpleFormat(date) {
-    const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    let now = new Date();
-    let outTimeFormat = formatDateWithMillisecond((now.getTime() - new Date(date).getTime()) / 1000);
-    var time = new Date(date).getTime();
-
-    if (outTimeFormat.dayNumber == 0)
-        return outTimeFormat.hourNumber + ':' + outTimeFormat.minuteNumber + ':' + outTimeFormat.secondNumber;
-    if (outTimeFormat.dayNumber > 0 && outTimeFormat.dayNumber < 1)
-        return outTimeFormat.hourNumber + ':' + outTimeFormat.minuteNumber + ':' + outTimeFormat.secondNumber;
-    if (outTimeFormat.dayNumber >= 1 && outTimeFormat.dayNumber < 365) {
-        return new Date(time).getDate() + ' ' + monthNames[new Date(time).getMonth()];
-    }
-    if (outTimeFormat.dayNumber > 365) {
-        return monthNames[new Date(time).getMonth()] + ' ' + new Date(time).getFullYear();
-    }
-}
-
 // Format: day hour minute second (1d5h45m78s)
 export function compactFormat(date) {
     let now = new Date();
     let outTime = formatDateWithMillisecond((now.getTime() - new Date(date).getTime()) / 1000);
-
-    // if (outTime.dayNumber > 49 )
-    //     return '+7weeks';
 
     let outTimeFormat = [];
     if (outTime.dayNumber > 0 && outTimeFormat.length < 2) outTimeFormat.push(outTime.dayNumber + 'd');
