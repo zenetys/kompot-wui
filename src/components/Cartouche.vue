b430d88f3faa32cc28e09a95e60fdfb70ef19bb6
Julien Thomas <jthomas@zenetys.com>
2023-07-19 18:43:08 +0200

    CLEANUP: cartouche: Avoid excessively long lines in template

--- src/components/Cartouche.vue
+++ src/components/Cartouche.vue
@@ -2,29 +2,36 @@
 
 <template>
     <div>
-        <v-chip :title="$t('hostUp')" small class="mr-1 elevation-2" color="green lighten-2" label>
-            {{ ($store.data.counters.hosts.up + $store.data.counters.hosts.pending) || '' }}
-        </v-chip>
-        <v-chip :title="$t('hostDown')" small class="mr-1 elevation-2" color="red lighten-2" label>
-            {{ $store.data.counters.hosts.down }}
-        </v-chip>
-        <v-chip :title="$t('hostUnreachable')" small class="mr-1 elevation-2" color="orange accent-3" label>
-            {{ $store.data.counters.hosts.unreachable }}
-        </v-chip>
+        <v-chip
+            id="khost_up" :title="$t('hostUp')"
+            small label class="mr-1 elevation-2" color="green lighten-2"
+        >{{ ($store.data.counters.hosts.up + $store.data.counters.hosts.pending) || '' }}</v-chip>
+        <v-chip
+            id="khost_down" :title="$t('hostDown')"
+            small label class="mr-1 elevation-2" color="red lighten-2"
+        >{{ $store.data.counters.hosts.down }}</v-chip>
+        <v-chip
+            id="khost_unreachable" :title="$t('hostUnreachable')"
+            small label class="mr-1 elevation-2" color="orange accent-3"
+        >{{ $store.data.counters.hosts.unreachable }}</v-chip>
         <span v-if="!$vuetify.breakpoint.smAndDown">|</span>
         <span v-else><v-divider class="mt-2 mb-2" /></span>
-        <v-chip :title="$t('hostUp')" small class="mr-1 ml-1 elevation-2" color="green lighten-2" label>
-            {{ ($store.data.counters.services.ok + $store.data.counters.services.pending) || '' }}
-        </v-chip>
-        <v-chip :title="$t('serviceWarning')" small class="mr-1 elevation-2" color="yellow darken-2" label>
-            {{ $store.data.counters.services.warning }}
-        </v-chip>
-        <v-chip :title="$t('serviceCritical')" small class="mr-1 elevation-2" color="red lighten-2" label>
-            {{ $store.data.counters.services.critical }}
-        </v-chip>
-        <v-chip :title="$t('serviceUnknown')" small class="mr-1 elevation-2" color="orange accent-3" label>
-            {{ $store.data.counters.services.unknown }}
-        </v-chip>
+        <v-chip
+            id="ksvc_ok" :title="$t('serviceOk')"
+            small label class="mr-1 ml-1 elevation-2" color="green lighten-2"
+        >{{ ($store.data.counters.services.ok + $store.data.counters.services.pending) || '' }}</v-chip>
+        <v-chip
+            id="ksvc_warning" :title="$t('serviceWarning')"
+            small label class="mr-1 elevation-2" color="yellow darken-2"
+        >{{ $store.data.counters.services.warning }}</v-chip>
+        <v-chip
+            id="ksvc_critical" :title="$t('serviceCritical')"
+            small label class="mr-1 elevation-2" color="red lighten-2"
+        >{{ $store.data.counters.services.critical }}</v-chip>
+        <v-chip
+            id="ksvc_unknown" :title="$t('serviceUnknown')"
+            small label class="mr-1 elevation-2" color="orange accent-3"
+        >{{ $store.data.counters.services.unknown }}</v-chip>
     </div>
 </template>
 
