5425f3c774cb6e83e1a65dc7cb3ba4b930f2c4be
Julien Thomas <jthomas@zenetys.com>
2024-12-03 14:35:55 +0100

    MINOR: zlink: Add link component able to handle route and href locations
    Simple component that render an anchor tag, allowing to pass routes
    via the "to" props or classic href links via the "href" props, just
    like vuetify would do with v-list-item or v-btn. Unlike the vuetify
    components, it gives a plain <a> tag, without any crappy style.

---
+++ src/components/ZLink.vue
@@ -0,0 +1,19 @@
+<template>
+    <a v-if="href" :href="href" :target="target" @click.stop>
+        <slot></slot>
+    </a>
+    <router-link v-else :to="to">
+        <slot></slot>
+    </router-link>
+</template>
+
+<script>
+export default {
+    name: 'ZLink',
+    props: {
+        href: { type: String, required: false },
+        target: { type: String, required: false },
+        to: { type: [ String, Object ], required: false },
+    },
+};
+</script>
